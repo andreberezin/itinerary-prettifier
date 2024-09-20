82e3b961d3bd155d425153d76b8caa610685b955
Andre Berezin <andreberezin@outlook.com>
2024-09-20 14:04:12 +0300

    quick commit

--- Prettifier.java
+++ Prettifier.java
@@ -1,14 +1,20 @@
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+// import java.io.BufferedWriter;
+// import java.io.FileWriter;
+
 public class Prettifier {
 	// Main method to start the program
 	public static void main(String[] args) {
-
-		// System.out.println("Prettifier");
 
 		if (args.length > 0 && args[0].equals("-h")) { // Check if -h option is provided
 			displayUsage();
 			return;
 		}
+
+		readFile(args[0]);
 
 	}
 
@@ -20,7 +26,20 @@ public class Prettifier {
 	}
 
 	// method to read the file
+	private static void readFile(String inputFilePath) {
+		// Read the file and process the data
 
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(inputFilePath));
+			while ((reader.readLine()) != null) {
+				System.out.println(reader.readLine()); // reader.readLine();
+			}
+			reader.close();
+		} catch (IOException e) {
+			System.out.println("Input not found");
+		}
+
+	}
 	// method to edit the file
 
 	// method to write the result to a new file
