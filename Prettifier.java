edcb8a415948de550d5838bfad6557efce5b7b6c
Andre Berezin <andreberezin@outlook.com>
2024-09-20 12:55:51 +0300

    quick commit

--- Prettifier.java
+++ Prettifier.java
@@ -1,7 +1,28 @@
 
 public class Prettifier {
+	// Main method to start the program
 	public static void main(String[] args) {
 
-		System.out.println("Prettifier");
+		// System.out.println("Prettifier");
+
+		if (args.length > 0 && args[0].equals("-h")) {
+			displayUsage();
+			return;
+		}
+
 	}
+
+	// method to display the usage
+	private static void displayUsage() {
+		System.out.println("Itinerary usage:");
+		System.out.println("$ java Prettifier.java ./input.txt ./output.txt ./airport-lookup.csv");
+		// Add more options as needed
+	}
+
+	// method to read the file
+
+	// method to edit the file
+
+	// method to write the result to a new file
+
 }
