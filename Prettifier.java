0cc5bfbcae1c1adb11f815357cf97fda04e17cc7
Andre Berezin <andreberezin@outlook.com>
2024-09-22 14:47:53 +0300

    modifying airport names

--- Prettifier.java
+++ Prettifier.java
@@ -16,7 +16,9 @@ public class Prettifier {
 			return;
 		}
 
-		writeFile(args[1], modifyInput(readFile(args[0])));
+		// ArrayList<String> airportList = readCSV(args[2]);
+
+		writeFile(args[1], modifyDates(modifyAirportNames(readFile(args[0]))));
 
 	}
 
@@ -48,34 +50,87 @@ public class Prettifier {
 		}
 
 		return inputList;
-
 	}
 
-	// method to convert the airport code using a CSV file
-	private static ArrayList<String> modifyInput(ArrayList<String> inputList) {
-		// String airpodCode = "";
-		// String airportName = "";
+	// method to convert the airport code
+	private static ArrayList<String> modifyAirportNames(ArrayList<String> inputList) {
 		int index = 0;
+		ArrayList<String> airportList = readCSV("airport-lookup.csv");
 
 		for (String word : inputList) {
 
 			if (word.startsWith("#")) { // would be safer with regex
-				inputList.set(index, "airportname1");
+				word = word.replaceAll("\\W+", "");
+
+				// find the match from airportList and replace
+				for (String airport : airportList) {
+					// find the match from airportList and replace
+
+					if (airport.contains(word)) {
+						String[] airportArray = airport.split(",");
+						// String airportName = airport.substring(0, airport.indexOf(","));
+						String destinationAirport = airportArray[0];
+						inputList.set(index, destinationAirport);
+						break;
+					}
+				}
 			}
 
-			if (word.startsWith("##")) { // would be safer with regex
-				inputList.set(index, "airportname2");
-			}
+			index++;
+		}
 
-			// if (inputList.get(index).matches("new line")) {
-			// inputList.set(index, "/n");
-			// }
+		return inputList;
+	}
+
+	// method to modify the dates
+
+	private static ArrayList<String> modifyDates(ArrayList<String> inputList) {
+		int index = 0;
+
+		for (String word : inputList) {
+			if (word.startsWith("D")) { // if word is date && word matches regex of date format
+				// Dates must be displayed in the output as DD-Mmm-YYYY format. E.g. "05 Apr
+				// 2007"
+				String date = "DATE";
+				inputList.set(index, date);
+			}
+			if (word.startsWith("T12")) { // if word is 12 hour time && word matches regex of date format
+				// must be displayed as "12:30PM (-02:00)".
+				String date = "T12";
+				inputList.set(index, date);
+			}
+			if (word.startsWith("T24")) { // if word is 24 hour time && word matches regex of date format
+				// must be displayed as "12:30 (-02:00)"
+				String date = "T24";
+				inputList.set(index, date);
+			}
 
 			index++;
 		}
 
 		System.out.println(inputList);
 		return inputList;
+	}
+
+	// method to read the csv file to an array list
+	private static ArrayList<String> readCSV(String airportCSV) {
+
+		ArrayList<String> airportList = new ArrayList<>();
+
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(airportCSV));
+			String line;
+			while ((line = reader.readLine()) != null) {
+				// airportList.addAll(Arrays.asList(line.split(" ")));
+				airportList.add(line);
+			}
+			reader.close();
+		} catch (IOException e) {
+			System.out.println("File not found");
+		}
+
+		// System.out.println(airportList);
+		return airportList;
 	}
 
 	// method to write the result to a new file
